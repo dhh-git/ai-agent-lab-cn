237933601ca1e473be57d6ec8f3cdbc86726ee2f
ex-donghh001 <ex-donghh001@cmhk.com>
2026-06-14 16:24:16 +0800

    ai-agent

--- .idea/app/agent_tool.py
+++ .idea/app/agent_tool.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 def get_current_time():
-    return  datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    return  datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 USER = {
